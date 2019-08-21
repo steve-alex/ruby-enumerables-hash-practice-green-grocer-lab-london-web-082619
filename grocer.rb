2e700439e9dac71bf687c4e2c859647a3cbe103a
Steve Alex <steve.alex@hotmail.co.uk>
2019-08-21 15:48:32 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -17,16 +17,36 @@ end
 def apply_coupons(cart, coupons)
   coupons.each do |coupon|
     item_name = coupon[:item]
-    if cart[item_name]
-      cart[item_name][:count] -= coupon[:num]
-      cart["#{coupon[:item]} W/COUPON"] = {:price => 0, :clearance => nil, :count => 0}
-      cart["#{coupon[:item]} W/COUPON"][:price] += coupon[:cost]/coupon[:num]
-      cart["#{coupon[:item]} W/COUPON"][:clearance] = cart[item_name][:clearance]
-      cart["#{coupon[:item]} W/COUPON"][:count] += coupon[:num]
-    else
+    unless cart[item_name] || cart[item_name][:count] <= 0
       next
+    else
+      if cart[item_name][:count] <= coupon[:num]
+        coupons_used = cart[item_name][:count]
+        if cart["#{coupon[:item]} W/COUPON"]
+          cart[item_name][:count] -= coupons_used
+          cart["#{coupon[:item]} W/COUPON"][:price] += coupon[:cost]/coupon[:num]
+          cart["#{coupon[:item]} W/COUPON"][:clearance] = cart[item_name][:clearance]
+          cart["#{coupon[:item]} W/COUPON"][:count] += coupons_used
+          cart[item_name][:count] = 0
+          coupon[:num] -= cart[item_name][:count]
+        else
+          cart["#{coupon[:item]} W/COUPON"] = {:price => coupon[:cost]/coupon[:num], :clearance => cart[item_name][:clearance], :count => coupons_used}
+          cart[item_name][:count] = 0
+          coupon[:num] -= cart[item_name][:count]
+
+        end
+      else
+        if cart["#{coupon[:item]} W/COUPON"]
+          cart["#{coupon[:item]} W/COUPON"][:price] += coupon[:cost]/coupon[:num]
+          cart["#{coupon[:item]} W/COUPON"][:clearance] = cart[item_name][:clearance]
+          cart["#{coupon[:item]} W/COUPON"][:count] += coupon[:num]
+        else
+          cart["#{coupon[:item]} W/COUPON"] = {:price => coupon[:cost]/coupon[:num], :clearance => cart[item_name][:clearance], :count => coupon[:num]}
+          cart[item_name][:count] -= coupon[:num]
+        end
+      end
     end
-  end  
+  end
   cart
 end
 
